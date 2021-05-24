c0ca8e9e02c26aefa82224c808b4845669ceef27
Cristhian Santos <70783719+CristhianFSantos@users.noreply.github.com>
2021-05-24 09:27:14 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Base64EncodeAndDecodeFile
+{
+    partial class Base64EncodeAndDecodeFile
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(159)))), ((int)(((byte)(56)))));
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(6, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Converter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.ForeColor = System.Drawing.Color.Coral;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selecione o arquivo que será convertido para base64:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.ForeColor = System.Drawing.Color.Coral;
+            this.groupBox2.Location = new System.Drawing.Point(12, 134);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 434);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cole o texto base64 que será convertido para arquivo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(10, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(292, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "_________________________________________________________";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.textBox5.ForeColor = System.Drawing.Color.White;
+            this.textBox5.Location = new System.Drawing.Point(10, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(292, 23);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox5_MouseClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ou selecione o arquivo TXT:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.textBox4.ForeColor = System.Drawing.Color.White;
+            this.textBox4.Location = new System.Drawing.Point(261, 293);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 23);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "pdf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(6, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Informe a extensão do arquivo (Exemplo: pdf)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(6, 360);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(292, 23);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selecione o diretório que o arquivo será salvo:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(159)))), ((int)(((byte)(56)))));
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(6, 405);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Converter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(6, 22);
+            this.textBox2.MaxLength = 999999999;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 139);
+            this.textBox2.TabIndex = 1;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Base64EncodeAndDecodeFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(56)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(341, 580);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Base64EncodeAndDecodeFile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Base64EncodeAndDecodeFile";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using MaterialSkin;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Base64EncodeAndDecodeFile
+{
+    public partial class Base64EncodeAndDecodeFile : Form
+    {
+        public FileInfo FileToBase64 { get; set; }
+        public FileInfo FileFromBase64 { get; set; }
+
+        public Base64EncodeAndDecodeFile()
+        {
+            InitializeComponent();
+          
+        }
+
+        #region CONVERTE PARA BASE 64
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+            FileInfo file = new(textBox1.Text);
+            FileToBase64 = file;
+            button1.Enabled = true;
+        }
+        private void ToBase64(FileInfo file)
+        {
+            try
+            {
+                string fileString = file.FullName;
+                string newFileName = file.Name.Replace(file.Extension, "");
+                string pathSave = fileString.Replace(file.Name, "");
+
+                if (File.Exists(fileString))
+                {
+                    byte[] objByte = File.ReadAllBytes(fileString);
+                    string fileEnd = Convert.ToBase64String(objByte);
+                    string pathAndFile = $@"{pathSave}{newFileName}_Base64.txt";
+                    File.WriteAllText(pathAndFile, fileEnd);
+                    MessageBox.Show($"Arquivo salvo em: {pathSave}");
+                }
+                else
+                {
+                    MessageBox.Show("Parâmetros inválidos!");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Parâmetros inválidos!");
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToBase64(FileToBase64);
+        }
+
+        #endregion
+
+        #region CONVERTE PARA ARQUIVO
+        private void textBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+            button2.Enabled = true;
+        }
+
+        private void FromBase64(string path)
+        {
+            try
+            {
+                var txtBase64 = textBox2.Text;
+                if ((txtBase64.Length >= 1 || File.Exists(FileFromBase64.FullName)) & Directory.Exists(path) & textBox4.Text.Length >= 1)
+                {
+                    if (FileFromBase64 == null & txtBase64.Length >= 1)
+                    {
+                        byte[] objByte = Convert.FromBase64String(txtBase64);
+                        File.WriteAllBytes(@$"{path}\arquivo_retorno_Base64.{textBox4.Text}", objByte);
+                        MessageBox.Show($"Arquivo salvo em: {path}");
+                    }
+                    else if(FileFromBase64.Length > 1 & txtBase64.Length <= 1)
+                    {
+                        if (File.Exists(FileFromBase64.FullName))
+                        {
+                            string file = File.ReadAllText(FileFromBase64.FullName);
+                            byte[] objByte = Convert.FromBase64String(file);
+                            File.WriteAllBytes(@$"{path}\arquivo_retorno_Base64.{textBox4.Text}", objByte);
+                            MessageBox.Show($"Arquivo salvo em: {path}");
+                        }
+                    }
+                    else
+                    {
+                        if (File.Exists(FileFromBase64.FullName))
+                        {
+                            string file = File.ReadAllText(FileFromBase64.FullName);
+                            byte[] objByte = Convert.FromBase64String(file);
+                            File.WriteAllBytes(@$"{path}\arquivo_retorno_Base64.{textBox4.Text}", objByte);
+                            MessageBox.Show($"Arquivo salvo em: {path}");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Parâmetros inválidos!");
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show($"Parâmetros inválidos!");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FromBase64(textBox3.Text);
+        }
+        private void textBox5_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox5.Text = openFileDialog2.FileName;
+            }
+            FileInfo file = new(textBox5.Text);
+            FileFromBase64 = file;
+        }
+        
+        #endregion
+
+    }
+}
